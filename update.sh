f8759765aecf4fe0be7bf59836b89e8741935b31
池下克彦 <ikeshita.katsuhiko@unipro.co.jp>
2018-01-06 11:01:28 +0900

    update.sh

--- update.sh
+++ update.sh
@@ -6,11 +6,12 @@ fi
 shift
 
 generate() {
+    NEW_VERSION=$(node -e 'console.log(require("./package.json").version)')
     (cat <<JS
 // ==UserScript==
 // @name         千年戦争アイギス・御城プロジェクト:RE 隠れてる時ミュート
 // @namespace    https://greasyfork.org/users/5795-ikeyan
-// @version      $(node -e 'console.log(require("./package.json").version)')
+// @version      $NEW_VERSION
 // @description  タブが隠れてる時やウィンドウが最小化しているときに消音します。
 // @author       ikeyan
 $(node generate-aigis-matches.js)
@@ -24,20 +25,23 @@ JS
     ) > index-new.js
 }
 
+set -x
 npm install
 generate
 if ! diff -q index.js index-new.js; then
     echo diff exists
     CURRENT_VERSION=$(grep '//\s*@version\b' index.js | head -1 | sed -Ee 's#^.*@version\s*##')
-    if [ $CURRENT_VERSION = $(node -e 'console.log(require("./package.json").version)') ]; then
+    if [ $CURRENT_VERSION = $NEW_VERSION ]; then
         echo "bump version"
-        npm version patch
+        npm version --no-git-tag-version -f patch
         echo "regenerate"
         generate
     fi
     diff --color index.js index-new.js
     mv index-new.js index.js
-    git add index.js
-    git commit -m "Updated index.js"
+    git add index.js package.json package-lock.json
+    git commit -m "Updated index.js to v$NEW_VERSION"
+    git tag "v$NEW_VERSION"
     git push
+    git push --tags
 fi
