f2c37ea9746e299961ce2b5f05437faac29c6332
池下克彦 <ikeshita.katsuhiko@unipro.co.jp>
2018-01-06 10:39:33 +0900

    update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,19 @@
-cat <<JS
+if [[ $1 != --pulled ]]; then
+    git pull
+    bash "$0" --pulled "$@"
+    exit
+fi
+shift
+
+generate() {
+    (cat <<JS
 // ==UserScript==
 // @name         千年戦争アイギス・御城プロジェクト:RE 隠れてる時ミュート
-// @namespace   https://greasyfork.org/users/5795-ikeyan
-// @version      $(cat version.txt)
+// @namespace    https://greasyfork.org/users/5795-ikeyan
+// @version      $(node -e 'console.log(require("./package.json").version)')
 // @description  タブが隠れてる時やウィンドウが最小化しているときに消音します。
 // @author       ikeyan
-// @match        http://assets.millennium-war.net/87e460b15025198b3551caa2beb66866108f5747/1ac385b3a7c489ee00e022824a3972ac
-// @match        http://assets.millennium-war.net/87e460b15025198b3551caa2beb66866108f5747/9602b5e3050180f4a828c0db76d66a80
+$(node generate-aigis-matches.js)
 // @match        http://assets.shiropro-re.net/html/Oshiro.html
 // @grant        none
 // @run-at       document-start
@@ -14,3 +21,23 @@ cat <<JS
 
 $(cat body.js)
 JS
+    ) > index-new.js
+}
+
+npm install
+generate
+if ! diff -q index.js index-new.js; then
+    echo diff exists
+    CURRENT_VERSION=$(grep '//\s*@version\b' index.js | head -1 | sed -Ee 's#^.*@version\s*##')
+    if [ $CURRENT_VERSION = $(node -e 'console.log(require("./package.json").version)') ]; then
+        echo "bump version"
+        npm version patch
+        echo "regenerate"
+        generate
+    fi
+    diff --color index.js index-new.js
+    mv index-new.js index.js
+    git add index.js
+    git commit -m "Updated index.js"
+    git push
+fi
