c8f5e9cf7773c3cfd03dc81874bf3a8134390ebe
池下克彦 <ikeshita.katsuhiko@unipro.co.jp>
2018-01-06 11:01:51 +0900

    Updated index.js to v0.3.1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         千年戦争アイギス・御城プロジェクト:RE 隠れてる時ミュート
-// @namespace   https://greasyfork.org/users/5795-ikeyan
-// @version      0.3.0
+// @namespace    https://greasyfork.org/users/5795-ikeyan
+// @version      0.3.1
 // @description  タブが隠れてる時やウィンドウが最小化しているときに消音します。
 // @author       ikeyan
-// @match        http://assets.millennium-war.net/87e460b15025198b3551caa2beb66866108f5747/1ac385b3a7c489ee00e022824a3972ac
-// @match        http://assets.millennium-war.net/87e460b15025198b3551caa2beb66866108f5747/9602b5e3050180f4a828c0db76d66a80
+// @match        http://assets.millennium-war.net/b4c204714d269c4be25802c3fe640d09e649aaed/5fd859d3183377288104adfeb2dcc5a4
+// @match        http://assets.millennium-war.net/b4c204714d269c4be25802c3fe640d09e649aaed/646d5320002b36b2862c8681de304eef
 // @match        http://assets.shiropro-re.net/html/Oshiro.html
 // @grant        none
 // @run-at       document-start
